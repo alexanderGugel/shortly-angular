90510fb2c36ddfcb6227e2a315a788748e4b7cda
Hack Reactor Students <alexander.gugel@gmail.com>
2014-07-02 17:04:54 -0700

    Implement basic templates

--- client/app/shorten/shorten.js
+++ client/app/shorten/shorten.js
@@ -1,14 +1,16 @@
 angular.module('shortly.shorten', [])
 
 .controller('ShortenController', function ($scope, $location, Links) {
-  // Your code here
   $scope.loading = false;
   $scope.link = {};
 
   $scope.addLink = function () {
     $scope.loading = true;
     Links.addLink($scope.link).then(function(){
+      console.log($scope.url);
       $scope.loading = false;
+    }).catch(function (error) {
+      throw error;
     });
   };
 });
